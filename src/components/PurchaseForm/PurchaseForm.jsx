c23f7db4398b4b7007fa3da0e05ff1fd67362eb6
Alex James <alexjames@Alexs-MacBook-Pro.local>
2019-11-16 17:54:42 -0600

    added product form

--- src/components/PurchaseForm/PurchaseForm.jsx
+++ src/components/PurchaseForm/PurchaseForm.jsx
@@ -12,13 +12,24 @@ export default class PurchaseForm extends Component {
                 <div className={style.line}></div>
             <h2 className={style.sectionTitle}>Place Your Order Today!</h2>
             <div className={style.contactForm}>
-                <h3 className={style.formTitle}>Contact Information</h3>
                 <form action="https://formspree.io/moqqdpnr" method="POST" className={style.form}>
-                  <input type="text" name="name" placeholder="Name" className={classy(style.input, style.name)}/>
-                  <input type="email" name="_replyto" placeholder="Email" className={classy(style.input, style.email)}/>
-                  <input type="phone" name="phoneNumber" placeholder="Phone Number" className={classy(style.input, style.phone)}/>
-                  <textarea name="comments" placeholder="What can we help you with?"className={classy(style.input, style.comment)}/>
-                  <input type="submit" value="Send" className={style.submit}/>
+                    <div className={style.contact}>
+                        <h3 className={style.formTitle}>Contact Information</h3>
+                        <input type="text" name="name" placeholder="Name" className={classy(style.input, style.name)}/>
+                        <input type="email" name="_replyto" placeholder="Email" className={classy(style.input, style.email)}/>
+                        <input type="phone" name="phoneNumber" placeholder="Phone Number" className={classy(style.input, style.phone)}/>
+                        <textarea name="comments" placeholder="What can we help you with?"className={classy(style.input, style.comment)}/>
+                    </div>
+                    <div className={style.productInfo}>
+                        <h3 className={style.formTitle}>Product Information</h3>
+                        <input type="text" name="product" placeholder="Product" className={classy(style.input, style.product)}/>
+                        <div className={style.specifications}>
+                            <input type="text" name="size" placeholder="Size" className={classy(style.input, style.size)}/>
+                            <input type="text" name="length" placeholder="Length" className={classy(style.input, style.length)}/>
+                        </div>
+                        <textarea name="requirements" placeholder="Additional Requirements"className={classy(style.input, style.comment)}/>
+                        <input type="submit" value="Submit" className={style.submit}/>
+                    </div>
                 </form>
             </div>
         </Section>
